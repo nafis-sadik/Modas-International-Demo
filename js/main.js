7092b002ae984b6598d265d4b237a160818ff91f
Nafis Sadik <nafis_sadik@outlook.com>
2020-10-06 17:49:28 +0600

    1. Contact Us developed 2. Multiple visual updates introduced

--- js/main.js
+++ js/main.js
@@ -5,10 +5,15 @@ const Routs = {
     LatestUpdates: '/partialViewes/latestUpdates.html',
     Services: '/partialViewes/services.html',
     Projects: '/partialViewes/projects.html',
-    About: '/partialViewes/about.html'
+    About: '/partialViewes/about.html',
+    Contact: '/partialViewes/Contact.html'
 }
 
+const NoLoadingScreenRouts = [Routs.Carousel, Routs.LatestUpdates]
+
 let loadPartialView = (HolderTagId, Url) => {
+    if(!NoLoadingScreenRouts.includes(Url))
+        $('#LoadingSpinner').fadeIn();
     let holdingTagId = (HolderTagId[0] == '#')? HolderTagId : '#' + HolderTagId;
     
     $.ajax({
@@ -16,9 +21,47 @@ let loadPartialView = (HolderTagId, Url) => {
         success: function (result) {
             $(holdingTagId).empty();
             $(holdingTagId).html(result);
+            if(!NoLoadingScreenRouts.includes(Url))
+                setTimeout(() => { $('#LoadingSpinner').fadeOut(); }, 1000);
         },
         error(response) {
             console.error(response);
         }
     });
+}
+
+$(document).ready(() => {
+    loadPartialView('Site-Body', Routs.Home);
+    $('#LoadingSpinner').fadeOut();
+
+    $('.home-btn').click((elem) => {
+        setActive(elem);
+        loadPartialView('Site-Body', Routs.Home);
+    });
+
+    $('#Services').click((elem) => {
+        setActive(elem);
+        loadPartialView('Site-Body', Routs.Services);
+    });
+    
+    $('#Projects').click((elem) => {
+        setActive(elem);
+        loadPartialView('Site-Body', Routs.Projects);
+    });
+    
+    $('#About').click((elem) => {
+        setActive(elem);
+        loadPartialView('Site-Body', Routs.About);
+    });
+    
+    $('#Contact').click((elem) => {
+        setActive(elem);
+        loadPartialView('Site-Body', Routs.Contact);
+    });
+});
+
+let setActive = (elem) => {
+    $('#navbarSupportedContent .nav-item').removeClass('active');
+    $(elem.currentTarget).parent().addClass('active');
+    $('#Site-NavBar').removeClass('active')
 }
